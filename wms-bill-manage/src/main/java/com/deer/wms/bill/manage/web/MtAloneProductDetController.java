23c3f07b5e01361b5c9b99fcce42420af8fad7f3
chnops <1159102911@qq.com>
2019-05-28 22:36:33 +0800

    修改判断条件

--- wms-bill-manage/src/main/java/com/deer/wms/bill/manage/web/MtAloneProductDetController.java
+++ wms-bill-manage/src/main/java/com/deer/wms/bill/manage/web/MtAloneProductDetController.java
@@ -467,20 +467,25 @@ public class MtAloneProductDetController {
 		}
 		PageHelper.startPage(params.getPageNum(), params.getPageSize());
 		List<MtAloneProductDet> list = mtAloneProductDetService.findDetilByProductId(params);
-		List<MtAloneExaminationDetails> listExaminationDetails=mtAloneProductDetService.findDetExaminationDetails(list.get(0).getWarehouseBarcode());
 		List<MtAloneProductDetExamDto>detExaminationDetailsList=new ArrayList<MtAloneProductDetExamDto>();
-		for(int i=0;i<list.size();i++){
-			MtAloneProductDetExamDto mtAloneProductDetExamDto=new MtAloneProductDetExamDto();
-			BeanUtils.copyProperties(list.get(i),mtAloneProductDetExamDto);
-			for(int j=0;j<listExaminationDetails.size();j++){
-				if(list.get(i).getWarehouseBarcode().equals(listExaminationDetails.get(j).getWarehouseBarcode())){
-					mtAloneProductDetExamDto.getDetExaminationDetailsList().add(listExaminationDetails.get(j));
+		if(!list.isEmpty()){
+			List<MtAloneExaminationDetails> listExaminationDetails=mtAloneProductDetService.findDetExaminationDetails(list.get(0).getWarehouseBarcode());
+			for(int i=0;i<list.size();i++){
+				MtAloneProductDetExamDto mtAloneProductDetExamDto=new MtAloneProductDetExamDto();
+				BeanUtils.copyProperties(list.get(i),mtAloneProductDetExamDto);
+				for(int j=0;j<listExaminationDetails.size();j++){
+					if(list.get(i).getWarehouseBarcode().equals(listExaminationDetails.get(j).getWarehouseBarcode())){
+						mtAloneProductDetExamDto.getDetExaminationDetailsList().add(listExaminationDetails.get(j));
+					}
 				}
+				detExaminationDetailsList.add(mtAloneProductDetExamDto);
 			}
-			detExaminationDetailsList.add(mtAloneProductDetExamDto);
+			PageInfo pageInfo = new PageInfo(detExaminationDetailsList);
+			return ResultGenerator.genSuccessResult(pageInfo);
+		}else{
+			PageInfo pageInfo = new PageInfo(list);
+			return ResultGenerator.genSuccessResult(pageInfo);
 		}
-		PageInfo pageInfo = new PageInfo(detExaminationDetailsList);
-		return ResultGenerator.genSuccessResult(pageInfo);
 	}
 
 	@ApiImplicitParams({

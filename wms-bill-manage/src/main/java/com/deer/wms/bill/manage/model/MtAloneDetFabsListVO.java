f24520fa05400a5e4bb324763eb7d62ee215bdfb
chnops <1159102911@qq.com>
2019-07-03 16:03:52 +0800

    优化明细瑕疵接口

--- wms-bill-manage/src/main/java/com/deer/wms/bill/manage/model/MtAloneDetFabsListVO.java
+++ wms-bill-manage/src/main/java/com/deer/wms/bill/manage/model/MtAloneDetFabsListVO.java
@@ -4,6 +4,38 @@ import java.util.ArrayList;
 
 public class MtAloneDetFabsListVO extends MtAloneProductDet{
 
+
+    private String productCode;//品号
+
+    private String productName;//产品名称
+
+    private String cellAlias;//货位别名
+
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCellAlias() {
+        return cellAlias;
+    }
+
+    public void setCellAlias(String cellAlias) {
+        this.cellAlias = cellAlias;
+    }
+
     private ArrayList<MtAloneExaminationDetails>fabricList=new ArrayList<MtAloneExaminationDetails>();
 
     public ArrayList<MtAloneExaminationDetails> getFabricList() {

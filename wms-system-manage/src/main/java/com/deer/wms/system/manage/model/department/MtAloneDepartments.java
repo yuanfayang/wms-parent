d33cd22df97a897140e54533b1e13966cf85c879
guotuanting <819187349@qq.com>
2019-07-15 14:27:33 +0800

    fix(BUG): 增加部门接口

---
+++ wms-system-manage/src/main/java/com/deer/wms/system/manage/model/department/MtAloneDepartments.java
@@ -0,0 +1,18 @@
+package com.deer.wms.system.manage.model.department;
+
+import java.util.List;
+
+public class MtAloneDepartments {
+
+    private List<MtAloneDepartment> mtAloneDepartments;
+
+    public List<MtAloneDepartment> getMtAloneDepartments() {
+        return mtAloneDepartments;
+    }
+
+    public void setMtAloneDepartments(List<MtAloneDepartment> mtAloneDepartments) {
+        this.mtAloneDepartments = mtAloneDepartments;
+    }
+
+
+}
---
+++ wms-system-manage/src/test/resources/generator/template/params.ftl
@@ -0,0 +1,9 @@
+package ${basePackage}.model;
+
+import com.deer.wms.project.seed.core.service.QueryParams;
+
+/**
+* Created by ${author} on ${date}.
+*/
+public class ${modelNameUpperCamel}Params extends QueryParams {
+}

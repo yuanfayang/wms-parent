c8100673c92f22060361fa8069e84d1aac6fec65
why <1075331873@qq.com>
2019-09-11 14:45:35 +0800

    供应商物品mt_alone_stock_product  ①添加②修改③删除④条件查询⑤导入（还有点问题）⑥导出功能已经实现

--- wms-produce-manage/src/test/java/generator/CodeGenerator.java
+++ wms-produce-manage/src/test/java/generator/CodeGenerator.java
@@ -24,7 +24,7 @@ public class CodeGenerator {
         configurer.setModuleName("/wms-produce-manage");
 
         //设置需要生成的表
-        configurer.getTables().add(new Table("mt_alone_materials_info", null, "id", "Integer"));
+        configurer.getTables().add(new Table("mt_alone_stock_product", null, "id", "Integer"));
 
         //生成代码
         Generator.genCode(configurer);
